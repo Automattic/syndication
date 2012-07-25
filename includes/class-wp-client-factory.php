db45ade1f550fb962b038b8f152d1132f482f5a4
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-07-25 09:29:30 +0530

    Ading all the files

---
+++ includes/class-wp-client-factory.php
@@ -0,0 +1,41 @@
+<?php
+
+// @TODO load all the classes dynamically
+include_once( dirname( __FILE__ ) . '/class-wp-xmlrpc-client.php' );
+include_once( dirname( __FILE__ ) . '/class-wp-rest-client.php' );
+
+class wp_client_factory {
+
+	public static function get_client( $transport_type, $site_ID ) {
+
+		$class = $transport_type . '_client';
+		if( class_exists($class) ) {
+			return new $class( $site_ID );
+		}
+
+		throw new Exception('transport class not found');
+
+	}
+
+	public static function display_client_settings( $site, $class ) {
+
+		if( class_exists($class) ) {
+			return $class::display_settings( $site );
+		}
+
+		throw new Exception('transport class not found');
+
+	}
+
+	public static function save_client_settings( $site_ID, $class ) {
+
+		$class = $_POST['transport_type'] . '_client';
+		if( class_exists($class) ) {
+			return $class::save_settings( $site_ID );
+		}
+
+		throw new Exception('transport class not found');
+
+	}
+
+}
