c1acd5cea68accfee189d3e3297d5f1b9a53d3c7
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-07-29 00:09:33 +0530

    new directory structure

---
+++ push-syndication-xmlrpc-client/push-syndication-xmlrpc-client.php
@@ -0,0 +1,14 @@
+<?php
+/*
+
+**************************************************************************
+
+Plugin Name:  Push Syndication XMLRPC Client
+Plugin URI:
+Description:  XMLRPC client for push syndication server
+Version:      1.0
+Author:       Automattic
+Author URI:   http://automattic.com/wordpress-plugins/
+License:      GPLv2 or later
+
+**************************************************************************/
