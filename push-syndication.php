6d6699ccd7b62af3799577f287e22771d0462233
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-08-20 11:02:41 +0530

    cleanup

--- push-syndication.php
+++ push-syndication.php
@@ -12,8 +12,8 @@ License:      GPLv2 or later
 
 **************************************************************************/
 
-require_once ( dirname( __FILE__ ) . '/includes/class-wpcom-push-syndication-server.php' );
 require_once ( dirname( __FILE__ ) . '/includes/class-wp-push-syndication-server.php' );
+require_once ( dirname( __FILE__ ) . '/includes/class-wpcom-push-syndication-server.php' );
 
 
 if ( !defined( 'PUSH_SYNDICATION_ENVIRONMENT' ) )
