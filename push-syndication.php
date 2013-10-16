29d7925f1253853530df4b6a7f5f4ee3774ef5c4
Mohammad Jangda <batmoo@gmail.com>
2013-10-16 05:42:33 +0000

    rm PUSH_SYNDICATION_ENVIRONMENT and related code
    Not super easy to hook into so let's just remove it allow alternate ways
    to hook in and override things.

--- push-syndication.php
+++ push-syndication.php
@@ -16,14 +16,7 @@ if ( ! defined( 'PUSH_SYNDICATE_KEY' ) )
 
 require_once ( dirname( __FILE__ ) . '/includes/class-wp-push-syndication-server.php' );
 
-if( apply_filters( 'syn_use_async_jobs', false ) )
-	require_once ( dirname( __FILE__ ) . '/includes/class-wpcom-push-syndication-server.php' );
-
-if ( !defined( 'PUSH_SYNDICATION_ENVIRONMENT' ) )
-	define( 'PUSH_SYNDICATION_ENVIRONMENT', 'WP' );
-
 if ( defined( 'WP_CLI' ) && WP_CLI )
 	require_once( dirname( __FILE__ ) . '/includes/class-wp-cli.php' );
 
-$push_syndication_server_class = PUSH_SYNDICATION_ENVIRONMENT . '_Push_Syndication_Server';
-$push_syndication_server = new $push_syndication_server_class();
+$push_syndication_server = new WP_Push_Syndication_Server;
