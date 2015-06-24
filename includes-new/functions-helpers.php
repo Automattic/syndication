0105ccaa120f2324bded293886018611bf7b28d0
James Mehorter <james@10up.com>
2015-06-24 16:32:42 -0400

    46 - Adjusting wp_error helper function
    Adjusted the throw_if_wp_error function to be an is_wp_error_do_throw function instead. The difference being that it now returns true or false, allowing it to be used interchangably with is_wp_error, i.e. if ( is_wp_error_do_throw( $something ) ) and still benefiting from the exeption being thrown.

--- includes-new/functions-helpers.php
+++ includes-new/functions-helpers.php
@@ -7,16 +7,20 @@ namespace Automattic\Syndication;
  * exception.
  *
  * @param $res
- * @return mixed
+ * @return bool
  * @throws \Exception
  *
  * @todo Handle WP_Error objects with multiple errors and codes.
  * @todo Create an exception object that is just for WP_Errors so we can catch
  * those specifically.
  */
-function throw_if_wp_error( $res ) {
+function is_wp_error_do_throw( $res ) {
 
 	if ( is_wp_error( $res ) ) {
 		throw new \Exception( $res->get_message(), $res->get_error_code() );
+
+		return true;
+	} else {
+		return false;
 	}
 }
