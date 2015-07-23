71e235dec955fae478bcebd770e78236c4c3b286
Adam Silverstein <adam@10up.com>
2015-07-23 09:38:37 -0600

    Use settings manager for pull_time_interval

--- includes-new/class-cron.php
+++ includes-new/class-cron.php
@@ -11,9 +11,10 @@ class Cron {
 
 	public function add_pull_time_interval( $schedules ) {
 
+		global $settings_manager;
 		// Adds the custom time interval to the existing schedules.
 		$schedules['syn_pull_time_interval'] = array(
-			'interval' => (int) get_option( 'syndication/pull_time_interval' ),
+			'interval' => (int) $settings_manager->get_setting( 'pull_time_interval' ),
 			'display' => __( 'Pull Time Interval', 'push-syndication' )
 		);
 
