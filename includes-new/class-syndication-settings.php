1e143cb5059a901c224d10c2cdb0973add55fa68
Adam Silverstein <adam@10up.com>
2015-07-30 13:28:03 -0600

    Add syndication encrypt/decript to settings helper class

--- includes-new/class-syndication-settings.php
+++ includes-new/class-syndication-settings.php
@@ -57,4 +57,40 @@ class Syndication_Settings {
 		return isset( $this->push_syndicate_settings[ $setting_id ] ) ? $this->push_syndicate_settings[ $setting_id ] : false;
 	}
 
+	static function syndicate_encrypt( $data ) {
+
+		$data = serialize( $data );
+		return base64_encode(
+			mcrypt_encrypt(
+				MCRYPT_RIJNDAEL_256,
+				md5( PUSH_SYNDICATE_KEY ),
+				$data,
+				MCRYPT_MODE_CBC,
+				md5( md5( PUSH_SYNDICATE_KEY ) )
+			)
+		);
+
+	}
+
+	static function syndicate_decrypt( $data ) {
+
+		$data = rtrim(
+			mcrypt_decrypt(
+				MCRYPT_RIJNDAEL_256 ,
+				md5( PUSH_SYNDICATE_KEY ) ,
+				base64_decode( $data ),
+				MCRYPT_MODE_CBC,
+				md5(
+					md5( PUSH_SYNDICATE_KEY )
+				)
+			), '\0'
+		);
+		if ( ! $data ) {
+			return false;
+		}
+
+		return @unserialize( $data );
+
+	}
+
 }
