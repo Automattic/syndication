7b5007ca5ea789676f9512a2fab78be4114a92eb
Adam Silverstein <adam@10up.com>
2015-08-05 23:47:53 -0600

    Manually include files instead of using autoloader

--- includes-new/class-bootstrap.php
+++ includes-new/class-bootstrap.php
@@ -5,6 +5,72 @@
 
 namespace Automattic\Syndication;
 
+/**
+ * Include all the files required for the plugin.
+ */
+
+// Functions
+require __DIR__ . '/functions-helpers.php';
+require __DIR__ . '/functions-template-tags.php';
+
+// Types
+require __DIR__ . '/types/class-post.php';
+require __DIR__ . '/types/class-term.php';
+
+// Custom Taxonomies
+require __DIR__ . '/custom-taxonomies/class-sitegroup-taxonomy.php';
+
+// Custom post types
+require __DIR__ . '/custom-post-types/class-site-post-type.php';
+
+// Classes
+require __DIR__ . '/class-client-manager.php';
+require __DIR__ . '/class-cron.php';
+require __DIR__ . '/class-legacy-hooks.php';
+require __DIR__ . '/class-puller.php';
+require __DIR__ . '/class-pusher.php';
+require __DIR__ . '/class-site-manager.php';
+require __DIR__ . '/class-syndication-logger.php';
+require __DIR__ . '/class-syndication-logger-viewer.php';
+require __DIR__ . '/class-syndication-runner.php';
+require __DIR__ . '/class-syndication-settings.php';
+require __DIR__ . '/class-syndication-site-auto-retry.php';
+require __DIR__ . '/class-syndication-site-failure-monitor.php';
+require __DIR__ . '/class-upgrade-tasks.php';
+require __DIR__ . '/class-syndication-event-counter.php';
+
+// Clients
+// @todo combine client files into single file
+
+// XML Pull
+require __DIR__ . '/clients/xml-pull/class-bootstrap.php';
+require __DIR__ . '/clients/xml-pull/class-client-options.php';
+require __DIR__ . '/clients/xml-pull/class-pull-client.php';
+require __DIR__ . '/clients/xml-pull/class-site-options.php';
+require __DIR__ . '/clients/xml-pull/class-walker-category-dropdown-multiple.php';
+
+// XML Push
+require __DIR__ . '/clients/xml-push/class-bootstrap.php';
+require __DIR__ . '/clients/xml-push/class-client-options.php';
+require __DIR__ . '/clients/xml-push/class-push-client.php';
+require __DIR__ . '/clients/xml-push/class-site-options.php';
+require __DIR__ . '/clients/xml-push/class-walker-category-dropdown-multiple.php';
+
+// RSS Pull
+require __DIR__ . '/clients/rss-pull/class-bootstrap.php';
+require __DIR__ . '/clients/rss-pull/class-client-options.php';
+require __DIR__ . '/clients/rss-pull/class-pull-client.php';
+require __DIR__ . '/clients/rss-pull/class-site-options.php';
+require __DIR__ . '/clients/rss-pull/class-walker-category-dropdown-multiple.php';
+
+// Admin
+require __DIR__ . '/admin/class-post-edit-screen.php';
+require __DIR__ . '/admin/class-settings-screen.php';
+require __DIR__ . '/admin/class-site-edit-screen.php';
+require __DIR__ . '/admin/class-site-list-screen.php';
+
+
+
 class Bootstrap {
 
 	/**
@@ -49,7 +115,6 @@ class Bootstrap {
 		new Syndication_Runner();
 
 		// Bootstrap individual built-in clients.
-		new Clients\Test\Bootstrap();
 		new Clients\XML_Pull\Bootstrap();
 		new Clients\RSS_Pull\Bootstrap();
 		new Clients\XML_Push\Bootstrap();
