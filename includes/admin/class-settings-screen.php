0f44a5673ce605e8c990e91b4b93298ca7549ba3
Derrick Tennant <derrick.tennant@automattic.com>
2016-05-11 13:27:43 -0700

    Updated `$_POST['push_syndicate_pull_now']` to reflect changes in button naming

--- includes/admin/class-settings-screen.php
+++ includes/admin/class-settings-screen.php
@@ -30,7 +30,8 @@ class Settings_Screen {
 	 * @return array              Validated settings.
 	 */
 	public function push_syndicate_settings_validate( $raw_settings ) {
-		if ( isset( $_POST['push_syndicate_pull_now'] ) && 'Pull Now' === $_POST['push_syndicate_pull_now'] ) {
+		vip_dump( $_POST );
+		if ( isset( $_POST['push_syndicate_pull_now'] ) && 'Pull Now & Save Changes' === $_POST['push_syndicate_pull_now'] ) {
 			\Automattic\Syndication\Syndication_Runner::pull_now_job();
 		}
 
