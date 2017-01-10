474d7f66db6e907efcd60dd84985689a12d2b5fa
Philip John <phil@philipjohn.co.uk>
2017-01-10 13:51:15 +0000

    Test editing sitegroups

--- tests/test-sitegroups.php
+++ tests/test-sitegroups.php
@@ -45,4 +45,27 @@ class SiteGroups extends WP_UnitTestCase {
 		$this->assertTrue( $deleted );
 
 	}
+
+	function test_edit_sitegroup() {
+
+		$sitegroup_taxonomy = 'syn_sitegroup';
+		$sitegroup_name = 'Site Group C';
+
+		$this->factory->term->create_object( array(
+			'taxonomy' => $sitegroup_taxonomy,
+			'name' => $sitegroup_name,
+		) );
+
+		$term = get_term_by( 'name', $sitegroup_name, $sitegroup_taxonomy );
+
+		$deleted = wp_update_term( $term->term_id, $term->taxonomy, array(
+			'name' => 'Site Group C1',
+			'slug' => 'site-group-c1',
+		) );
+
+		$modified_term = get_term_by( 'name', 'Site Group C1', $sitegroup_taxonomy );
+
+		$this->assertInstanceOf( 'WP_Term', $modified_term );
+
+	}
 }
