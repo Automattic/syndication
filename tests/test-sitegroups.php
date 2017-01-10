4a9ce094d64dd523c4aaa928927d28144b5ae408
Philip John <phil@philipjohn.co.uk>
2017-01-10 13:45:06 +0000

    Add first Sitegroup test

---
+++ tests/test-sitegroups.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Syndication
+ */
+
+/**
+ * Sample test case.
+ */
+class SiteGroups extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_add_new_sitegroup() {
+
+		$sitegroup_taxonomy = 'syn_sitegroup';
+		$sitegroup_name = 'Site Group A';
+
+		$this->factory->term->create_object( array(
+			'taxonomy' => $sitegroup_taxonomy,
+			'name' => $sitegroup_name,
+		) );
+
+		$term = get_term_by( 'name', $sitegroup_name, $sitegroup_taxonomy );
+
+		$this->assertInstanceOf( 'WP_Term', $term );
+	}
+}
