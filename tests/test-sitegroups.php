b9dcc3220f69a8515f2d43f2b60ac81166e88513
Philip John <phil@philipjohn.co.uk>
2017-01-10 13:47:59 +0000

    Test deleting sitegroups

--- tests/test-sitegroups.php
+++ tests/test-sitegroups.php
@@ -27,4 +27,22 @@ class SiteGroups extends WP_UnitTestCase {
 
 		$this->assertInstanceOf( 'WP_Term', $term );
 	}
+
+	function test_delete_sitegroup() {
+
+		$sitegroup_taxonomy = 'syn_sitegroup';
+		$sitegroup_name = 'Site Group B';
+
+		$this->factory->term->create_object( array(
+			'taxonomy' => $sitegroup_taxonomy,
+			'name' => $sitegroup_name,
+		) );
+
+		$term = get_term_by( 'name', $sitegroup_name, $sitegroup_taxonomy );
+
+		$deleted = wp_delete_term( $term->term_id, $term->taxonomy );
+
+		$this->assertTrue( $deleted );
+
+	}
 }
